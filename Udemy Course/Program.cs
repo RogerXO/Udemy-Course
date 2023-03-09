b24e164e2aaac18c8e74118f4a13412de1704a17
RogerXavier <rogerxavier10@gmail.com>
2023-03-09 16:39:14 -0300

    #68 - Nullable

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,13 +1,41 @@
-﻿
-using System.Drawing;
+﻿using System;
 
 namespace Udemy_Course
-{ 
-Console.WriteLine("#66 - reference type and value type ");
-Points p; //Error - needs to inicilize the struct before use it.
-    p.X = 10;
-p.Y = 20;
-Console.WriteLine(p);
-p= new Points();
-    Console.WriteLine(p);
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("#68 - Nullable");
+            double? x = null;
+            double? y = 10;
+
+            Console.WriteLine(x.GetValueOrDefault());
+            Console.WriteLine(y.GetValueOrDefault());
+
+            Console.WriteLine(x.HasValue);
+            Console.WriteLine(y.HasValue);
+
+            if (x.HasValue)
+                Console.WriteLine(x.Value);
+            else
+                Console.WriteLine("X is null");
+
+            if (y.HasValue)
+                Console.WriteLine(y.Value);
+            else
+                Console.WriteLine("Y is null");
+
+            Console.WriteLine();
+
+            //Operador de coalescência nula
+            double? a = null;
+            double? b = 20;
+
+            double? c = a ?? b;
+            // It will try to set "c" as "a", but if the "a" be null, it will set the "c" as "b"
+
+            Console.WriteLine(c);
+        }
+    }
 }
