309c2a4faa88f7c780e286076193a86570ac6100
RogerXavier <rogerxavier10@gmail.com>
2023-03-09 15:02:13 -0300

    #66 - reference type and value type (struct)

---
+++ Udemy Course/Point.cs
@@ -0,0 +1,13 @@
+﻿namespace Udemy_Course
+{
+    struct Points
+    {
+        public double X;
+        public double Y;
+
+        public override string ToString()
+        {
+            return $"({X}, {Y})";
+        }
+    }
+}
--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, c#! dsadasdsad");
+﻿
+using System.Drawing;
+
+namespace Udemy_Course
+{ 
+Console.WriteLine("#66 - reference type and value type ");
+Points p; //Error - needs to inicilize the struct before use it.
+    p.X = 10;
+p.Y = 20;
+Console.WriteLine(p);
+p= new Points();
+    Console.WriteLine(p);
+}
