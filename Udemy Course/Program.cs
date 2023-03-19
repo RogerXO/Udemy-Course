c95b513960255fec04ec9c8f8dc82174cfed123f
RogerXavier <rogerxavier10@gmail.com>
2023-03-19 16:31:35 -0300

    #71 Vecto Exercise

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -6,29 +6,56 @@ namespace Udemy_Course
     {
         static void Main(string[] args)
         {
-            Console.Write("How many people?");
-            int n = int.Parse(Console.ReadLine());
+            int availableRooms = 10;
+            Tenant[] rooms = new Tenant[availableRooms];
 
-            double[] heightVect = new double[n]; 
-
-            for (int i = 0; i<n; i++)
+            Console.Write("How many rooms will be rented? ");
+            int rentedRooms = int.Parse(Console.ReadLine());
+            if (rentedRooms > 10)
             {
-                Console.Write($"What is the #{i + 1} person height?");
-                heightVect[i] = double.Parse(Console.ReadLine()); 
+                Console.WriteLine("Sorry, We have just 10 availables rooms :(");
+                return;
             }
-
-            double sum = 0;
-            for (int i = 0; i<n; i++)
-            {
-                sum += heightVect[i];
-            }
-            double averageHeight = sum / n;
-            Console.WriteLine($"Average Height: {averageHeight.ToString("F2")}");
 
             Console.WriteLine();
-            Console.WriteLine("Second exercise");
 
-            Vect2.Exercise();
+            for (int i = 0; i < rentedRooms; i++)
+            {
+                Console.WriteLine($"Rent #{i+1}");
+                Console.Write("Name: ");
+                string name = Console.ReadLine();
+                Console.Write("Email: ");
+                string email = Console.ReadLine();
+                Console.Write("Room (0 - 9): ");
+                int room = int.Parse(Console.ReadLine());
+                if (room > 9)
+                {
+                    Console.WriteLine("Please, select a room between 0 and 9");
+                    room = int.Parse(Console.ReadLine());
+                    
+                    if (room > 9)
+                    {
+                        Console.WriteLine("Sorry, We do not have so many rooms :(");
+                        return;
+                    }
+                }
+
+                Console.WriteLine();
+
+                Tenant student = new Tenant(name, email, room);
+                rooms[room] = student;
+            }
+
+            Console.WriteLine();
+
+            Console.WriteLine("Busy rooms:");
+            for (int i = 0; i < availableRooms; i++)
+            {
+                if (rooms[i] != null)
+                {
+                    Console.WriteLine(rooms[i]);
+                }
+            }
         }
     }
 }
