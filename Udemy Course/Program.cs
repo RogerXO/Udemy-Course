b35e6d26fe8ef2d6737d3aed64ec9146a236ab05
RogerXavier <rogerxavier10@gmail.com>
2023-03-19 17:58:05 -0300

    #71 - params

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,61 +1,23 @@
 ﻿using System;
 
-namespace Udemy_Course
-{
-    class Program
+int Sum(int[] numbers) {
+    int sum = 0;
+    for (int i  = 0; i < numbers.Length; i++)
     {
-        static void Main(string[] args)
-        {
-            int availableRooms = 10;
-            Tenant[] rooms = new Tenant[availableRooms];
-
-            Console.Write("How many rooms will be rented? ");
-            int rentedRooms = int.Parse(Console.ReadLine());
-            if (rentedRooms > 10)
-            {
-                Console.WriteLine("Sorry, We have just 10 availables rooms :(");
-                return;
-            }
-
-            Console.WriteLine();
-
-            for (int i = 0; i < rentedRooms; i++)
-            {
-                Console.WriteLine($"Rent #{i+1}");
-                Console.Write("Name: ");
-                string name = Console.ReadLine();
-                Console.Write("Email: ");
-                string email = Console.ReadLine();
-                Console.Write("Room (0 - 9): ");
-                int room = int.Parse(Console.ReadLine());
-                if (room > 9)
-                {
-                    Console.WriteLine("Please, select a room between 0 and 9");
-                    room = int.Parse(Console.ReadLine());
-                    
-                    if (room > 9)
-                    {
-                        Console.WriteLine("Sorry, We do not have so many rooms :(");
-                        return;
-                    }
-                }
-
-                Console.WriteLine();
-
-                Tenant student = new Tenant(name, email, room);
-                rooms[room] = student;
-            }
-
-            Console.WriteLine();
-
-            Console.WriteLine("Busy rooms:");
-            for (int i = 0; i < availableRooms; i++)
-            {
-                if (rooms[i] != null)
-                {
-                    Console.WriteLine(rooms[i]);
-                }
-            }
-        }
+        sum += numbers[i];
     }
+    return sum;
 }
+
+int SumWithParams(params int[] numbers)
+{
+    int sum = 0;
+    for (int i = 0; i < numbers.Length; i++)
+    {
+        sum += numbers[i];
+    }
+    return sum;
+}
+
+Console.WriteLine(Sum(new int[] { 1, 2, 3 }));
+Console.WriteLine(SumWithParams(1, 2, 3));
