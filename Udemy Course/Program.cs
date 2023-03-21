11b71a981ac5439e638fbe129337ca79ecb353fd
RogerXavier <rogerxavier10@gmail.com>
2023-03-21 13:53:23 -0300

    #76 - List declaration and instantiation

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,26 +1,4 @@
 ﻿using System;
-using Udemy_Course;
 
-
-
-Console.WriteLine("Without ref");
-int a = 10;
-UsingRefAndOut.TripleNoRef(a);
-Console.WriteLine(a);
-
-Console.WriteLine();
-
-//The variable passed with "ref" needs to be inicialized;
-Console.WriteLine("With ref");
-int b = 10;
-UsingRefAndOut.TripleWithRef(ref b);
-Console.WriteLine(b);
-
-Console.WriteLine();
-
-//The variable passed with "out" doesnt need to be inicialized;
-Console.WriteLine("With out");
-int c = 10;
-int result;
-UsingRefAndOut.TripleWithOut(c, out result);
-Console.WriteLine(result);
+List<string> batata = new List<string>() {"batata", "repolho"};
+Console.WriteLine(batata[1]);
