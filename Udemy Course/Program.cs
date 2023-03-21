4d150c751fd9fc8bc6bcf0d1d4363a30443dad7a
RogerXavier <rogerxavier10@gmail.com>
2023-03-21 15:08:38 -0300

    #77 - List Methods

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,4 +1,75 @@
 ﻿using System;
 
-List<string> batata = new List<string>() {"batata", "repolho"};
-Console.WriteLine(batata[1]);
+List<string> list = new List<string>() { "Ana", "Tamara", "Leticia", "Carlos", "Jairo" };
+
+list.Add("Amanda");
+list.Insert(2, "Jussara");
+
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
+
+Console.WriteLine();
+Console.WriteLine($"List count: {list.Count()}");
+
+Console.WriteLine();
+Console.WriteLine($"First 'A': {list.Find(x => x[0] == 'A')}");
+
+Console.WriteLine();
+Console.WriteLine($"Last 'A': {list.FindLast(x => x[0] == 'A')}");
+
+Console.WriteLine();
+Console.WriteLine($"Find Index 'T': {list.FindIndex(x => x[0] == 'T')}");
+
+Console.WriteLine();
+Console.WriteLine($"Find Last Index 'A': {list.FindLastIndex(x => x[0] == 'A')}");
+
+Console.WriteLine();
+Console.WriteLine("FindAll: x[2] == 'a'");
+
+List<string> filteredList = list.FindAll(x => x[1] == 'a');
+foreach (string obj in filteredList)
+{
+    Console.WriteLine(obj);
+}
+
+Console.WriteLine();
+Console.WriteLine("Remove Jussara");
+
+list.Remove("Jussara");
+
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
+
+Console.WriteLine();
+Console.WriteLine("RemoveAll");
+list.RemoveAll(x => x[1] == 'a');
+
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
+
+Console.WriteLine();
+Console.WriteLine("Remove At");
+
+list.RemoveAt(2);
+
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
+
+
+Console.WriteLine();
+Console.WriteLine("RemoveRange");
+
+list.RemoveRange(0, 1);
+
+foreach (string obj in list)
+{
+    Console.WriteLine(obj);
+}
