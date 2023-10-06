155d9c3fa1992ca07e4b420a9ad89aab890a729c
RogerXavier <rogerxavier10@gmail.com>
2023-10-06 12:19:43 -0300

    #221 - Exercice (first build)

---
+++ Udemy Course/Entities/Teacher.cs
@@ -0,0 +1,34 @@
+﻿namespace Udemy_Course.Entities
+{
+    internal class Teacher
+    {
+        public string Name { get; set; }
+        private SortedSet<string> _courses = new() { "A", "B", "C"};
+        private SortedSet<Student> _students = new();
+
+        public Teacher(string name)
+        {
+            Name = name;
+        }
+
+        public void AddCourse(string courseName)
+        {
+            _courses.Add(courseName);
+        }
+
+        public SortedSet<string> GetCourses()
+        {
+            return _courses;
+        }
+
+        public void AddStudent(Student student)
+        {
+            _students.Add(student);
+        }
+
+        public int GetStudentsCount()
+        {
+            return _students.Count();
+        }
+    }
+}
--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,19 +1,28 @@
 ﻿using Udemy_Course.Entities;
 
-HashSet<Product> products = new();
-products.Add(new Product("TV", 900));
-products.Add(new Product("Notebook", 1200));
+try
+{
+    Teacher teacher = new("Alex");
+    SortedSet<string> courses = teacher.GetCourses();
 
-Product prod = new("Notebook", 1200);
-// If the reference type Product doesnt implements GetHashCode and Equals it will return false because in this case HashSet will compare the object memory reference, and as they are two differents instanced values they have two differents memory reference.
-// But if GetHashCode and Equals methods are implementeds in Product, HashSet will compare the content
-Console.WriteLine(products.Contains(prod));
+    teacher.AddCourse("A");
+    teacher.AddCourse("B");
+    teacher.AddCourse("C");
 
+    foreach (string course in courses)
+    {
+        Console.Write($"How many students for course {course}? ");
+        int studentsQuantity = int.Parse(Console.ReadLine());
 
-HashSet<Point> points = new();
-points.Add(new Point(1, 2));
-points.Add(new Point(5, 10));
-
-Point point = new(5,10);
-Console.WriteLine(points.Contains(point));
-// Point was implemented as struct and in this case the HashSet compare the itens content
+        for (int i = 0; i < studentsQuantity; i++)
+        {
+            int studentCode = int.Parse(Console.ReadLine());
+            teacher.AddStudent(new Student(studentCode));
+        }
+    }
+    Console.WriteLine($"Total students: {teacher.GetStudentsCount()}");
+}
+catch (IOException e)
+{
+    Console.WriteLine(e.Message);
+}
