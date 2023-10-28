5e9afc1f4bac0159473fd18ff54ab111ff18ef8b
RogerXavier <rogerxavier10@gmail.com>
2023-10-28 11:39:27 -0300

    #226 - Extension methods

---
+++ Udemy Course/Extension/StringExtension.cs
@@ -0,0 +1,19 @@
+﻿// namespace Udemy_Course.Extension
+// You can create the extension method in the same namespace of the type that you want to you
+namespace System
+{
+    static class StringExtension
+    {
+        public static string Cut(this string thisObj, int count)
+        {
+            if (thisObj.Length <= count)
+            {
+                return thisObj;
+            } 
+            else
+            {
+                return thisObj.Substring(0,count) + "...";
+            }
+        }
+    }
+}
--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,36 +1,7 @@
-﻿string path = @"C:\\Users\\User\\Documents\\poc.txt";
+﻿DateTime dt = new(2023, 10, 28, 11, 21, 45);
+Console.WriteLine(dt.ElapsedTime());
 
-try
-{
-    Console.WriteLine($"The path is: {path}");
+Console.WriteLine();
 
-    Dictionary<string, int> candidates = new();
-
-    using (StreamReader sr = File.OpenText(path))
-    {
-        while (!sr.EndOfStream)
-        {
-            string[] votingRecord = sr.ReadLine().Split(",");
-            string candidate = votingRecord[0];
-            int votes = int.Parse(votingRecord[1]);
-
-            if (candidates.ContainsKey(candidate))
-            {
-                candidates[candidate] += votes;
-            }
-            else
-            {
-                candidates.Add(candidate, votes);
-            }
-        }
-    }
-
-    foreach (KeyValuePair<string,int> item in candidates)
-    {
-        Console.WriteLine($"{item.Key}: {item.Value}");
-    }
-}
-catch (IOException e)
-{
-    Console.WriteLine(e.Message);
-}
+string s1 = "Good morning dear students!";
+Console.WriteLine(s1.Cut(10));
