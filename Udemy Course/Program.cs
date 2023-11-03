d0a29f6e900c1efc9a9c4653a14256fc180d647d
RogerXavier <rogerxavier10@gmail.com>
2023-11-03 11:01:56 -0300

    #228 - Inline lambda expression

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -6,9 +6,7 @@ list.Add(new Product("Tv", 900));
 list.Add(new Product("Notebook", 1200));
 list.Add(new Product("Tablet", 450));
 
-Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-
-list.Sort(comp);
+list.Sort((p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper()));
 
 foreach(Product item in list) {
     Console.WriteLine(item);
