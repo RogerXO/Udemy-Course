9bf64572db478ff68565a7fdff364e722db2b861
RogerXavier <rogerxavier10@gmail.com>
2023-11-03 10:58:30 -0300

    #228 - Simple method reference as parameter

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -6,8 +6,13 @@ list.Add(new Product("Tv", 900));
 list.Add(new Product("Notebook", 1200));
 list.Add(new Product("Tablet", 450));
 
-list.Sort();
+list.Sort(CompareProducts);
 
 foreach(Product item in list) {
     Console.WriteLine(item);
+}
+
+static int CompareProducts(Product p1, Product p2)
+{
+    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 }
