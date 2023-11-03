e38069c4ebaa739db0d0559643089e5ea4ce54ac
RogerXavier <rogerxavier10@gmail.com>
2023-11-03 11:01:28 -0300

    #228 - Lambda expresion to delegate variable

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -6,7 +6,7 @@ list.Add(new Product("Tv", 900));
 list.Add(new Product("Notebook", 1200));
 list.Add(new Product("Tablet", 450));
 
-Comparison<Product> comp = CompareProducts;
+Comparison<Product> comp = (p1, p2) => p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
 
 list.Sort(comp);
 
@@ -14,7 +14,3 @@ foreach(Product item in list) {
     Console.WriteLine(item);
 }
 
-static int CompareProducts(Product p1, Product p2)
-{
-    return p1.Name.ToUpper().CompareTo(p2.Name.ToUpper());
-}
