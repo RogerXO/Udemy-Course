f9568d2eb62567090b80611ab3f86b7e0a900aa2
RogerXavier <rogerxavier10@gmail.com>
2023-12-05 21:48:00 -0300

    #235 - Introduction to LINQ

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,41 +1,13 @@
-﻿using Udemy_Course.Entities;
+﻿using System.Linq;
 
-namespace Udemy_Couse
+//Specify the datasource
+int[] numbers = new int[] {2,3,4,5 };
+
+//Define the query expression
+IEnumerable<int> result = numbers.Where(x => x % 2 == 0).Select(x => x * 10);
+
+//Execute the query
+foreach (int x in result)
 {
-    //delegate
-    delegate void BinaryNumericOperation(double n1, double n2);
-
-    class Program
-    {
-        static void Main(string[] args)
-        {
-            List<Product> list = new()
-            {
-                new Product("Tv", 900),
-                new Product("Mouse", 50),
-                new Product("Tablet", 350),
-                new Product("HD Case", 89.90)
-            };
-
-            Func<Product, string> func = NameUpper;
-            Func<Product, string> func1 = p => p.Name.ToUpper();
-
-
-            // List<string> result = list.Select(NameUpper).ToList();
-            // List<string> result = list.Select(func).ToList();
-            //List<string> result = list.Select(func1).ToList();
-            List<string> result = list.Select(p => p.Name.ToUpper()).ToList();
-
-
-            foreach (string item in result)
-            {
-                Console.WriteLine(item);
-            }
-        }
-
-        static string NameUpper(Product p)
-        {
-            return p.Name.ToUpper();
-        }
-    }
+    Console.WriteLine(x);
 }
