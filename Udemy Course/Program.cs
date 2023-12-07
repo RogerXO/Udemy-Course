b6836508f0c963f3a0028964beca7e08e4b6463b
RogerXavier <rogerxavier10@gmail.com>
2023-12-07 18:29:50 -0300

    #238 - Demo - LINQ com lambda - Full Class

--- Udemy Course/Program.cs
+++ Udemy Course/Program.cs
@@ -1,10 +1,9 @@
-﻿using System.Linq;
-using Udemy_Course.Entities;
+﻿using Udemy_Course.Entities;
 
 static void Print<T>(string message, IEnumerable<T> list)
 {
     Console.WriteLine($"- {message} -");
-    foreach(var item in list)
+    foreach (var item in list)
     {
         Console.WriteLine(item);
     }
@@ -46,8 +45,8 @@ IEnumerable<Product> r5 = r4.Skip(2).Take(4);
 Print("Tier 1 ordered by price then by name, skip 2 take 4", r5);
 
 // Single() method returns the only element of a sequence and throw an exception if there is more than one in the sequence or if it be empty
-// var r10 = products.Where(p => p.Price > 30000).Single();
-// Console.WriteLine(r10);
+// var r6 = products.Where(p => p.Price > 30000).Single();
+// Console.WriteLine(r6);
 
 Product? r6 = products.FirstOrDefault();
 Console.WriteLine("First or default test1: " + r6);
@@ -62,3 +61,37 @@ Console.WriteLine("Single or default test1: " + r8);
 
 Product? r9 = products.Where(p => p.Id == 30).SingleOrDefault();
 Console.WriteLine("Single or default test2:" + r9);
+
+Console.WriteLine();
+
+double r10 = products.Max(p => p.Price);
+Console.WriteLine("Max price: " + r10);
+
+double r11 = products.Min(p => p.Price);
+Console.WriteLine("Min price: " + r11);
+
+double r12 = products.Where(p => p.Category.Id == 1).Sum(p => p.Price);
+Console.WriteLine("Category 1 Sum prices: " + r12);
+
+double r13 = products.Where(p => p.Category.Id == 1).Average(p => p.Price);
+Console.WriteLine("Category 1 Average prices: " + r13);
+
+double r14 = products.Where(p => p.Category.Id == 5).Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+Console.WriteLine("Category 5 Average prices: " + r14);
+
+double r15 = products.Where(p => p.Category.Id == 1).Select(p => p.Price).Aggregate((prev, cur) => prev + cur);
+Console.WriteLine("Category 1 aggregate sum: " + r15);
+
+Console.WriteLine();
+
+IEnumerable<IGrouping<Category, Product>> r16 = products.GroupBy(p => p.Category);
+
+foreach (IGrouping<Category, Product> group in r16)
+{
+    Console.WriteLine("Category " + group.Key.Name + ":");
+    foreach(Product p in group)
+    {
+        Console.WriteLine(p);
+    }
+    Console.WriteLine();
+}
